645611b9eb5a4be3330c8dad60d9aef2ef9e8017
Nolan Kuenzi <mr.nolank@gmail.com>
2019-06-09 21:54:19 -0700

    project structure update

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+require('dotenv').config();
 
 // data-base
 const mongoose = require('mongoose');
 
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true }).catch((error) => { console.log(error); });
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }).catch((error) => { console.log(error); });
 
 const app = express();
 const routes = require('./routes');
@@ -20,7 +20,7 @@ app.use(cors({optionSuccessStatus: 200}));
  
 // Point static path to dist
 
-app.use('/', express.static(path.join(__dirname, '..', 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 /** Get port from environment and store in Express. */
